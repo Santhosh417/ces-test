5f081094a980335af6d524b742c5312ce5970722
Aparna Dinesh <adineshshylaja@unomaha.edu>
2021-04-04 02:39:49 -0500

    Update for static page,logout
    Updated test for static pages, logout and deployed application

--- unitTest_Userlogin.py
+++ unitTest_Userlogin.py
@@ -12,30 +12,31 @@ class CES_ATS_LOGIN(unittest.TestCase):
     def test_ces(self):
         driver = self.driver
         driver.maximize_window()
-        driver.get("http://localhost:8080")
-        #driver.get("https://ces.herokuapp.com/")
+        #driver.get("http://localhost:8080")
+        driver.get("https://jolly-shockley-7553c8.netlify.app/")
+        time.sleep(5)
+        elem = driver.find_element_by_link_text("Login").click()
         time.sleep(3)
-        elem = driver.find_element_by_link_text("Log in").click()
+        elem=  driver.find_element_by_xpath("/html/body/div/div[2]/div/div/div[2]/div/div/div/form/div[1]/div/input")
+        elem.send_keys("admin")
+        elem = driver.find_element_by_xpath("/html/body/div/div[2]/div/div/div[2]/div/div/div/form/div[2]/div/input")
+        elem.send_keys("admin")
         time.sleep(3)
-        elem = driver.find_element_by_xpath("/html/body/div/div[3]/div/div/div/div[2]/div/div/div/form/div[1]/div/input")
-        elem.send_keys("advisor")
-        elem = driver.find_element_by_xpath("/html/body/div/div[3]/div/div/div/div[2]/div/div/div/form/div[2]/div/input")
-        elem.send_keys("instagram")
-        time.sleep(3)
-        elem= driver.find_element_by_xpath("/html/body/div/div[3]/div/div/div/div[2]/div/div/div/button").click()
+        elem= driver.find_element_by_xpath("/html/body/div/div[2]/div/div/div[2]/div/div/div/button").click()
         time.sleep(3)
         try:
-           # elem= driver.find_element_by_xpath("/html/body/div/div[3]/div/p") #verifies successful login using xpath to (div class)message "Hello.."
+            elem= driver.find_element_by_link_text("Search") #verifies successful login using link text for search button
             time.sleep(3)
 
-            elem = driver.find_element_by_link_text("Contact Us")
-            elem = driver.find_element_by_link_text("Contact Us").click()
-            #elem = driver.find_element_by_tag_name('h3') -- change this to a tag on contact us page
+            elem = driver.find_element_by_link_text("About us")
+            elem = driver.find_element_by_link_text("About us").click()
+            elem = driver.find_element_by_tag_name('h3')
             time.sleep(3)
 
             #code for logout
             # click on username on the right side of screen
-            elem = driver.find_element_by_xpath("/html/body/div/div[1]/div/div/div[3]/div/button").click()
+            elem = driver.find_element_by_xpath("/html/body/div/div[1]/div/div[3]/div/button").click()
+
             time.sleep(3)
             # select dropdown for logout
             elem= driver.find_element_by_link_text("Log out").click()
