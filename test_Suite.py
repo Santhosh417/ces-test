32293d76e28feaed6e7f01a1750b3e0ca9f84bf3
Aparna Dinesh <adineshshylaja@unomaha.edu>
2021-04-27 01:09:23 -0500

    Included more unit tests

--- test_Suite.py
+++ test_Suite.py
@@ -4,6 +4,10 @@ from tests.unitTest_Userlogin import CES_ATS_LOGIN
 from tests.unitTest_search import CES_ATS_SEARCH
 from tests.unitTest_addenrollment import CES_ATS_ADDENROLL
 from tests.unitTest_deleteenrollment import CES_ATS_DELETEENROLL
+#test for edit
+from tests.unitTest_adminenrollment import CES_ATS_ADMINENROLLMENT
+from tests.unitTest_admincourse import CES_ATS_COURSE
+from tests.unitTest_adminstudent import CES_ATS_STUDENT
 
 
 if __name__ == "__main__":
@@ -11,9 +15,13 @@ if __name__ == "__main__":
     suite = TestSuite((
 
         loader.loadTestsFromTestCase(CES_ATS_LOGIN),
-        loader.loadTestsFromTestCase(CES_ATS_SEARCH),
+        loader.loadTestsFromTestCase(CES_ATS_SEARCH), #includes download plan as well
         loader.loadTestsFromTestCase(CES_ATS_ADDENROLL),
-        loader.loadTestsFromTestCase(CES_ATS_DELETEENROLL)
+        loader.loadTestsFromTestCase(CES_ATS_DELETEENROLL),
+        #edit goes here
+        loader.loadTestsFromTestCase(CES_ATS_ADMINENROLLMENT),
+        loader.loadTestsFromTestCase(CES_ATS_COURSE),
+        loader.loadTestsFromTestCase(CES_ATS_STUDENT)
 
     ))
     runner = TextTestRunner(verbosity=2)
