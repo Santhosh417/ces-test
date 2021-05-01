d892567402f412dce92a7d6c7820317804f65828
Aparna Dinesh <adineshshylaja@unomaha.edu>
2021-05-01 15:02:29 -0500

    update delete enrollment
    update delete enrollment

--- unitTest_deleteenrollment.py
+++ unitTest_deleteenrollment.py
@@ -6,36 +6,39 @@ from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 
 
-class CES_ATS_ADDENROLL(unittest.TestCase):
+class CES_ATS_DELETEENROLL(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome()
 
     def test_ces(self):
         driver = self.driver
         driver.maximize_window()
-        driver.get("http://localhost:8081")
-        #driver.get("https://jolly-shockley-7553c8.netlify.app/")
-        #time.sleep(5)
+        #driver.get("http://localhost:8081")
+        driver.get("https://jolly-shockley-7553c8.netlify.app/")
+        time.sleep(3)
         elem = driver.find_element_by_link_text("Login").click()
         #time.sleep(3)
         elem=  driver.find_element_by_xpath("/html/body/div/div[2]/div/div/div[2]/div/div/div/form/div[1]/div/input")
         elem.send_keys("admin")
         elem = driver.find_element_by_xpath("/html/body/div/div[2]/div/div/div[2]/div/div/div/form/div[2]/div/input")
-        elem.send_keys("instagram")
+        elem.send_keys("admin")
         time.sleep(3)
         elem= driver.find_element_by_xpath("/html/body/div/div[2]/div/div/div[2]/div/div/div/button").click()
         time.sleep(3)
         try:
             elem= driver.find_element_by_xpath("/html/body/div/div[2]/div[1]/div/div[2]/div[1]/div/input")
-            elem.send_keys("12345")
+            elem.send_keys("27867799")
             time.sleep(3)
             #click on search
             elem= driver.find_element_by_xpath("/html/body/div/div[2]/div[1]/div/div[2]/div[2]/a").click()
             time.sleep(3)
 
             #click on delete for a course
-            elem =driver.find_element_by_xpath("/html/body/div/div[2]/div[2]/div[2]/div/table/tbody/tr[2]/td[7]/button[2]").click()
-            # elem =driver.find_element_by_link_text("Delete").click()
+            # old elem =driver.find_element_by_xpath("/html/body/div/div[2]/div[2]/div[2]/div/table/tbody/tr[3]/td[7]/button[2]").click()
+            elem =driver.find_element_by_xpath("/html/body/div/div[2]/div[2]/div/div[2]/div/table/tbody/tr[2]/td[7]/button[2]").click()
+            #elem = driver.find_element_by_link_text("Delete")
+            #elem = driver.find_element_by_link_text("Delete").click()
+            time.sleep(2)
             obj = driver.switch_to.alert
             time.sleep(3)
             obj.accept()
@@ -60,8 +63,7 @@ class CES_ATS_ADDENROLL(unittest.TestCase):
 
 
 def tearDown(self):
-    #self.driver.close()
-    self.driver.quit()
+    self.driver.close()
 
 
 if __name__ == '__main__':
